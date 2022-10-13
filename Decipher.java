07610f64b8939ba07335f6846677eccee2acde0f
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-10-13 09:30:50 -0500

    Initial commit

---
+++ Decipher.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Decipher{
+	
+	final String ENCRYPTED_FILE = "";
+	final String PLAINTEXT_FILE = "";
+	
+	public static void main(String[] args){
+		
+		
+		
+	}
+}
